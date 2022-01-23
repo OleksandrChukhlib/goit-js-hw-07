9720a68c9b631ab5209d0f2e853d287c6296be1e
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2022-01-23 08:48:26 +0200

    finished hw7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,44 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
-
 const galleryContainer = document.querySelector('.gallery');
-const picturesMarkup = createGallery(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup )
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(createGallery(galleryItems));
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
-function createGallery(galleryItems) {
-    return galleryItems.map(({preview, original, description})=> {
-        return `  
+function createGalleryMarkup(images) { 
+    return images.map(({ preview, original, description }) => {
+        return `
         <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img 
-      class="gallery__image" 
-      src="${preview}" 
-      data-source="${original}" 
-      alt="${description}" />
-    </a>
-  </div>`;
-    }).join("");
-
-
-   
-
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join(''); 
 }
 
-// console.log(galleryItems);
+function onGalleryContainerClick(event) { 
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) { 
+        return;
+    }
+    const urlLargeImg = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${urlLargeImg}" width="800" height="600">`)
+    instance.show();
+    if (instance) { 
+        document.addEventListener("keydown", event => {
+            if (event.code === 'Escape') { 
+                instance.close();
+            }
+        });
+    }
+}
